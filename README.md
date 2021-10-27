8ecb14abbb7d4021f5fab67e4e81cbb2dfdb014d
Tony Locke <tlocke@tlocke.org.uk>
2021-10-27 20:27:26 +0100

    Add journey

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 ## Home Site
 
-To view and generate locally do:
+Local setup:
+
+* install pelican: `pip install pelican[markdown]`
+
+Local view:
 
 * `pelican -lr`
 * View at http://localhost:8000/
--- pelicanconf.py
+++ pelicanconf.py
@@ -2,15 +2,15 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'Tony Locke'
-SITENAME = 'Tony Locke'
-SITEURL = ''
+AUTHOR = "Tony Locke"
+SITENAME = "Tony Locke"
+SITEURL = ""
 
-PATH = 'content'
+PATH = "content"
 
-TIMEZONE = 'Europe/London'
+TIMEZONE = "Europe/London"
 
-DEFAULT_LANG = 'en'
+DEFAULT_LANG = "en"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -28,24 +28,24 @@ DEFAULT_PAGINATION = 10
 DISPLAY_CATEGORIES_ON_MENU = False
 DISPLAY_PAGES_ON_MENU = False
 MENUITEMS = (
-    ('Archive', '/archives.html'),
-    ('Twitter', 'http://twitter.com/t_locke'),
-    ('GitHub', 'https://github.com/tlocke')
+    ("Archive", "/archives.html"),
+    ("Twitter", "http://twitter.com/t_locke"),
+    ("GitHub", "https://github.com/tlocke"),
 )
 
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
 
-OUTPUT_PATH = 'docs'
+OUTPUT_PATH = "docs"
 # THEME = "/home/tlocke/home/themes/notmyidea"
-THEME = "/home/tlocke/home/themes/simple"
-YEAR_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/index.html'
-MONTH_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/{date:%b}/index.html'
-DIRECT_TEMPLATES = (
-    'index', 'tags', 'categories', 'archives', 'period_archives'
-)
+THEME = "themes/simple"
+YEAR_ARCHIVE_SAVE_AS = "posts/{date:%Y}/index.html"
+MONTH_ARCHIVE_SAVE_AS = "posts/{date:%Y}/{date:%b}/index.html"
+DIRECT_TEMPLATES = ("index", "tags", "categories", "archives", "period_archives")
 
-STATIC_PATHS = ['images', 'extra/CNAME']
-EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'}, }
+STATIC_PATHS = ["images", "extra/CNAME"]
+EXTRA_PATH_METADATA = {
+    "extra/CNAME": {"path": "CNAME"},
+}
 
-READERS = {'html': None}
+READERS = {"html": None}
